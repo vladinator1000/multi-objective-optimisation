056cca34792800308c8478712720371e174a99a2
Vlady Veselinov <vladkodmc@gmail.com>
2017-10-30 02:16:50 +0000

    Readme, video and cleanup GA

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # multi-objective-optimisation
+
+Suppose we're developing a software, we have a list of requirements,
+their costs and a list of customers with desired requirements ordered by wishes.
+Each customer has a weight. Each requirement has a cost and score. Using a genetic algorithm
+this project is trying to find solutions using NSGA2 selection.
+Trying to minimise cost and maximise score.
+
+Using the [DEAP](https://github.com/DEAP/deap) Evolutionary Algorithms Python library.
+Main paper for reference: [Deb et al.](http://ieeexplore.ieee.org/document/996017/)
+Reference for [DEAP multi-objective-optimisation](https://github.com/lmarti/evolutionary-computation-course/blob/master/AEC.06%20-%20Evolutionary%20Multi-Objective%20Optimization.ipynb)
+
+## To Run:
+### 1. Install [pipenv](https://github.com/kennethreitz/pipenv)
+```bash
+$ pip install pipenv
+```
+
+### 2. cd to project directory and:
+```bash
+$ pipenv install
+```
+
+```bash
+$ pipenv shell
+```
+```bash
+$ python src/geneticAlgorithm.py
+```
+Wait for the GA to run, you'll see an animation at the end, similar to the [video](ParetoFrontsAnimation.mp4) in the project root.
+
+
+#### If you prefer using Anaconda:
+You'll need to install [DEAP](https://github.com/DEAP/deap) globally:
+```bash
+$ pip install deap
+```
+
+cd to project directory and:
+
+```bash
+$ python src/geneticAlgorithm.py
+```
+
+✨🍰✨
--- src/geneticAlgorithm.py
+++ src/geneticAlgorithm.py
@@ -1,7 +1,10 @@
 from deap import algorithms, base, benchmarks, tools, creator
 from random import randint
 from numpy import dot
-
+from matplotlib import pyplot, animation
+from copy import deepcopy
+from pandas import DataFrame
+from seaborn import color_palette
 
 from data import data
 from score import getScoreVector
@@ -33,7 +36,7 @@ toolbox.register('attr_bool', randint, 0, 1)
 toolbox.register('individual', tools.initRepeat, creator.Individual, toolbox.attr_bool, n = howManyRequirements)
 toolbox.register('population', tools.initRepeat, list, toolbox.individual)
 
-# This is the fitness function
+# Fitness Function
 def getScoreAndCost(requirementVec = [], scoreVec = scoreVector, costVec = costVector):
 	score = dot(scoreVec, requirementVec)
 	cost = dot(costVec, requirementVec)
@@ -45,22 +48,73 @@ toolbox.register('mate', tools.cxTwoPoint)
 toolbox.register('mutate', tools.mutFlipBit, indpb = 0.05)
 toolbox.register('select', tools.selNSGA2)
 
-# Initialise population with 'n' size
-population = toolbox.population(n = 10)
+# Set GA parameters
+toolbox.pop_size = 10
+toolbox.max_gen = 10
+toolbox.mut_prob = 0.1
+
+def runGA(toolbox, stats = None, verbose = False):
+	pop = toolbox.population(n = toolbox.pop_size)
+	pop = toolbox.select(pop, len(pop))
+
+	# Run evolutionary algorithm
+	# http://deap.readthedocs.io/en/master/api/algo.html#deap.algorithms.eaMuPlusLambda
+	return algorithms.eaMuPlusLambda(
+		pop,
+		toolbox,
+		mu = toolbox.pop_size, 
+		lambda_ = toolbox.pop_size, 
+		cxpb = 1 - toolbox.mut_prob,
+		mutpb = toolbox.mut_prob, 
+		stats = stats, 
+		ngen = toolbox.max_gen, 
+		verbose = verbose
+	)
 
 
-# Run the Genetic Algorithm
-for gen in range(10):
-	# Crossover and Mutation
-	offspring = algorithms.varAnd(population, toolbox, cxpb = 0.9, mutpb = 0.1)
+# Store individuals in logbook
+stats = tools.Statistics()
+stats.register("pop", deepcopy)
 
-	# Evaluate Fitnesses
-	fitnesses = toolbox.map(toolbox.evaluate, offspring)
+# Run the algorithm
+result, logbook = runGA(toolbox, stats = stats)
 
-	# Assign fitness to each individual in offspring
-	for fit, ind in zip(fitnesses, offspring):
-		ind.fitness.values = fit
 
-	# Selection
-	population = toolbox.select(offspring, k = len(population))
+plot_colors = color_palette("Set1", n_colors=20)
 
+def animate(frame_index, logbook):
+	ax.clear()
+	fronts = tools.emo.sortLogNondominated(
+		logbook.select('pop')[frame_index], 
+		len(logbook.select('pop')[frame_index])
+	)
+
+	for i, individuals in enumerate(fronts):
+		par = [toolbox.evaluate(ind) for ind in individuals]
+		dataFrame = DataFrame(par)
+
+		dataFrame.plot(
+			ax = ax,
+			kind = 'scatter',
+			label = 'Front ' + str(i + 1), 
+			x = dataFrame.columns[0],
+			y = dataFrame.columns[1], 
+			color=plot_colors[i]
+		)
+		
+	ax.set_title('Generation: ' + str(frame_index))
+	ax.set_xlabel('Score (trying to maximise)')
+	ax.set_ylabel('Cost (trying to minimise)')
+	
+	return []
+
+fig = pyplot.figure()
+ax = fig.gca()
+
+anim = animation.FuncAnimation(fig, lambda i: animate(i, logbook), 
+	frames=len(logbook),
+	interval=15, 
+	blit=True
+)
+
+pyplot.show()
